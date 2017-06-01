08da78df44a581c8b480f47aff23621d23aeeaa9
Felipe Shi Iu Wu <fsiw15@inf.ufpr.br>
2017-06-01 17:28:18 -0300

    problema do lider errado e do choose lider forever
    Signed-off-by: Felipe Shi Iu Wu <fsiw15@inf.ufpr.br>

--- peer.py
+++ peer.py
@@ -5,20 +5,47 @@ TCP_PORTS = [5000, 5001, 5002, 5003]
 TCP_IP = '127.0.0.1'
 TCP_PORT = TCP_PORTS[peer_id]
 BUFFER_SIZE = 1024  # Normally 1024, but we want fast response
-MESSAGE = "tuts " + sys.argv[1]
-
+MESSAGE = sys.argv[1]
+lider = 5
+lider_on = 0
 
 def Listening(s):
     s.bind((TCP_IP, TCP_PORT))
-
+    global lider
     while 1:
         s.listen(1)
         conn, addr = s.accept()
-        data = conn.recv(BUFFER_SIZE)
-        if not data: continue
-        print "received data:", data
+        try:
+            data = conn.recv(BUFFER_SIZE, socket.MSG_OOB)
+        except socket.error, value:
+            data = None
+        if data:
+            if int(data) < lider:
+                lider = int(data)
+        else:
+            data = conn.recv(BUFFER_SIZE)
+            if not data: continue
+            print "received data:", data
 
     conn.close()
+
+def check_lider(lider_on):
+    if lider_on == 0:
+        return 0
+    else:
+        return 1
+
+def choose_lider():
+    global lider
+    lider = 5
+    for i in TCP_PORTS:
+        try:
+            so = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            so.connect((TCP_IP, i))
+            so.send(MESSAGE, socket.MSG_OOB)
+            so.close()
+        except:
+            pass
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -32,7 +59,16 @@ while(True):
     time.sleep(2)
     for i in range(0,4):
         if i != peer_id:
-            so = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            so.connect((TCP_IP, TCP_PORTS[i]))
-            so.send(MESSAGE)
-            so.close()
+            try:
+                so = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                so.connect((TCP_IP, TCP_PORTS[i]))
+                so.send(MESSAGE)
+                so.close()
+            except:
+                if i == lider:
+                    print 'lider dead'
+                    choose_lider()
+    if not check_lider(lider_on):
+        choose_lider()
+        lider_on = 1
+    print lider
